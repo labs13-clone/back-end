1720fafa2d2c5c39ab1956c44433b85fe2428ffa
Anubhav311 <anubhavbhambri1@gmail.com>
2019-06-06 03:42:27 +0530

    Completed endpoints for adding and getting challenges

--- config/challengesRouter.js
+++ config/challengesRouter.js
@@ -9,11 +9,17 @@ const router = express.Router();
 
 // Replace with Challenge Routes
 
-router.post('/:id', (req, res) => {
+// Allow any registered user to create a challenge
+// Double check the approved column is false
+// Validate format of payload
+router.post('/', auth, (req, res) => {
     const challenge = {
         ...req.body,
-        created_by: req.params.id
+        created_by: req.headers.user.id,
+        approved: false
     }
+
+    //todo: validation of format of payload
     challengesApi.insert(challenge)
         .then(dbRes => {
             res.status(201).send(dbRes);
@@ -25,16 +31,33 @@ router.post('/:id', (req, res) => {
         })
 });
 
-router.get('/', (req, res) => {
-    challengesApi.getMany()
-        .then(dbRes => {
-            res.status(200).send(dbRes);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message
-            });
+// Query parameters can be used to filter results
+// By default it returns all approved challenges
+// Any registered user can access this endpoint
+// Regular users should only be able to access approved challenges no matter if they created them or not, and unapproved challenges that they created
+// Only admins can access all challenges
+// Returns an array of challenges
+router.get('/', auth, (req, res) => {
+    const filter = req.query
+    if(req.headers.role === "user") {
+        if(filter.approved === undefined) {
+            filter.approved = true;
+        } else if (filter.approved === false) {
+            filter.created_by = req.headers.user.id    
+        }
+    } else if (filter.approved === undefined) {
+        filter.approved = true;
+    }
+
+    challengesApi.getMany(filter)
+    .then(dbRes => {
+        res.status(200).send(dbRes);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message
         });
+    });
 });
 
 module.exports = router;
