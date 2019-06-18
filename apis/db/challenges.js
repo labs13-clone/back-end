e068634bb0ed43aaa11021c5fa8064e6a0cada12
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-06-18 11:55:49 -0500

    started implementing middleware and fixing bugs found

--- apis/db/challenges.js
+++ apis/db/challenges.js
@@ -38,20 +38,98 @@ function update(selector = null, payload) {
                 .where({
                     id
                 }).first();
-        });
+        }).catch(err => console.log(err));
 }
 
 //Get multiple challenges in the database
 //Filterable by sending in an object literal that matches the challenges schema
 function getMany(filter = {}) {
 
-    //Filters can be on different tables...
-    //Filters can also necessitate different knex query builder functions
-    //Here we start with an initial query
-    //And dynamically build on top of it.
+    //Parse filter and edit the query
+    const queryBuilder = parseFilter(filter);
+
+    //Finish building the query
+    return queryBuilder
+        //Then insert the challenges applicable categories in a nested array
+        .then(challenges => {
+
+            //Map over each challenge
+            const parsed = challenges.map(async challenge => {
+
+                console.log(challenge.created_by)
+
+                //Retrieve category info in an array
+                const challengeCategories = await db.select(
+                        'categories.id',
+                        'categories.name'
+                    )
+                    .from('categories')
+                    .leftJoin('challenges_categories', 'categories.id', 'challenges_categories.categories_id')
+                    .where({
+                        'challenges_categories.challenge_id': challenge.id
+                    });
+
+                //Add categories property on response
+                return {
+                    ...challenge,
+                    categories: challengeCategories
+                }
+            });
+
+            return Promise.all(parsed);
+        });
+
+}
+
+//Get a single challenge object
+//Filterable by sending in an object literal that matches the challenges schema
+function getOne(filter = null) {
+
+    if (!filter) return new Error('No filter provided for the query');
+
+    //Parse filter and edit query
+    const queryBuilder = parseFilter(filter);
+
+    //getOne(), so only return one object
+    queryBuilder.first();
+
+    return queryBuilder
+        .where(filter)
+        .then(async challenge => {
+
+            //Sometimes IDs that do not exist are looked up by the middleware
+            //If the ID does not exist then the following code will cause an error.
+            if(challenge !== undefined) {
+
+                const challengeCategories = await db.select(
+                        'categories.id',
+                        'categories.name'
+                    )
+                    .from('categories')
+                    .leftJoin('challenges_categories', 'categories.id', 'challenges_categories.categories_id')
+                    .where({
+                        'challenges_categories.challenge_id': challenge.id
+                    });
+    
+                return {
+                    ...challenge,
+                    categories: challengeCategories
+                }
+            }
+
+        });
+}
+
+//Filters can be on different tables...
+//Filters can also necessitate different knex query builder functions
+//Here we start with an initial query
+//And dynamically build on top of it.
+function parseFilter(filter) {
+
     const queryBuilder = db
         .select(
             'challenges.id',
+            'challenges.created_by',
             'challenges.approved',
             'challenges.title',
             'challenges.description',
@@ -98,11 +176,12 @@ function getMany(filter = {}) {
                 queryBuilder.whereBetween('difficulty', difficulty);
 
             }
-            
+
             //Else for all other query parameters in the challenges table
             else {
                 //Assign table
                 filter[`challenges.${key}`] = filter[key];
+
                 //Delete invalid property that doesn't have the table
                 deleteKey(key);
             }
@@ -177,75 +256,10 @@ function getMany(filter = {}) {
         delete filter[key];
     }
 
-    //Finish building the query
-    return queryBuilder
-        //Apply the parsed filter to the query builder
-        .where(filter)
-        //Then insert the challenges applicable categories in a nested array
-        .then(challenges => {
+    console.log(filter)
 
-            //Map over each challenge
-            const parsed = challenges.map(async challenge => {
+    //Apply the parsed filter to the query builder
+    queryBuilder.where(filter)
 
-                //Retrieve category info in an array
-                const challengeCategories = await db.select(
-                        'categories.id',
-                        'categories.name'
-                    )
-                    .from('categories')
-                    .leftJoin('challenges_categories', 'categories.id', 'challenges_categories.categories_id')
-                    .where({
-                        'challenges_categories.challenge_id': challenge.id
-                    });
-
-                //Add categories property on response
-                return {
-                    ...challenge,
-                    categories: challengeCategories
-                }
-            });
-
-            return Promise.all(parsed);
-        });
-
-}
-
-//Get a single challenge object
-//Filterable by sending in an object literal that matches the challenges schema
-function getOne(filter = null) {
-    if (!filter) return new Error('No filter provided for the query');
-    return db.select(
-            'challenges.id',
-            'challenges.approved',
-            'challenges.title',
-            'challenges.description',
-            'challenges.tests',
-            'challenges.skeleton_function',
-            'challenges.solution',
-            'challenges.difficulty'
-        )
-        .from('challenges')
-        .leftJoin('challenges_categories', 'challenges.id', 'challenges_categories.challenge_id')
-        .leftJoin('categories', 'challenges_categories.categories_id', 'categories.id')
-        .where(filter)
-        .groupBy('challenges.id')
-        .orderBy('challenges.id')
-        .first()
-        .then(async challenge => {
-
-            const challengeCategories = await db.select(
-                    'categories.id',
-                    'categories.name'
-                )
-                .from('categories')
-                .leftJoin('challenges_categories', 'categories.id', 'challenges_categories.categories_id')
-                .where({
-                    'challenges_categories.challenge_id': challenge.id
-                });
-
-            return {
-                ...challenge,
-                categories: challengeCategories
-            }
-        });
+    return queryBuilder;
 }
